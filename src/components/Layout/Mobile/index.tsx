4ec0e5fd90f0d9ef15844cd7f795c3b2d2b4f5c0
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-10-25 02:21:21 +0300

    Clean up the navigation props

--- src/components/Layout/Mobile/index.tsx
+++ src/components/Layout/Mobile/index.tsx
@@ -1,46 +1,40 @@
 import React, { useState } from "react";
 import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
-import RestoreIcon from "@mui/icons-material/Restore";
+import { Link as RouterLink } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import { useHistory } from "react-router-dom";
 import routes from "../../../constants/routes";
 
-const Layout: React.FC = () => {
-  const history = useHistory();
-  const [value, setValue] = useState<string>(routes.home);
+const Mobile: React.FC = () => {
+  const [value, setValue] = useState<string>(window.location.pathname);
 
-  const handleOnPageChange = (
-    event: React.SyntheticEvent,
-    newValue: string
-  ) => {
-    history.push(newValue);
-    setValue(newValue);
+  const navProps = (route: string) => {
+    return { to: route, value: route, component: RouterLink };
   };
 
   return (
     <Paper
       elevation={3}
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
-      <BottomNavigation showLabels value={value} onChange={handleOnPageChange}>
+      <BottomNavigation
+        value={value}
+        onChange={(e, newValue) => setValue(newValue)}>
         <BottomNavigationAction
-          label="Recents"
-          value={routes.account.view}
-          icon={<RestoreIcon />}
+          icon={<HomeIcon />}
+          {...navProps(routes.home)}
         />
         <BottomNavigationAction
-          label="Favorites"
-          value={routes.home}
           icon={<FavoriteIcon />}
+          {...navProps(routes.account.view)}
         />
         <BottomNavigationAction
-          label="Nearby"
-          value={routes.newPosts.all}
           icon={<LocationOnIcon />}
+          {...navProps(routes.newPosts.all)}
         />
       </BottomNavigation>
     </Paper>
   );
 };
 
-export default Layout;
+export default Mobile;

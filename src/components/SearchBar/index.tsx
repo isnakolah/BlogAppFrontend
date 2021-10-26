20ec841181f9afb72c83673f721ae0f29e491219
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-10-27 01:07:46 +0300

    Make the search bar to be an autocomplete field

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,47 +1,4 @@
-import { alpha, InputBase, styled } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+import { TextField, Autocomplete } from "@mui/material";
 
 type Props = {
   fullWidth?: boolean;
@@ -49,19 +6,37 @@ type Props = {
 
 const SearchBar: React.FC<Props> = ({ fullWidth = false }) => {
   return (
-    <>
-      <Search>
-        <SearchIconWrapper>
-          <SearchIcon />
-        </SearchIconWrapper>
-        <StyledInputBase
-          placeholder="Search…"
-          inputProps={{ "aria-label": "search" }}
-          style={{ width: fullWidth ? "100%" : undefined }}
+    <Autocomplete
+      freeSolo
+      id="free-solo-2-demo"
+      disableClearable
+      options={top100Films.map((option) => option.title)}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          fullWidth={fullWidth}
+          size="small"
+          variant="outlined"
+          placeholder="Search..."
+          InputProps={{
+            ...params.InputProps,
+            type: "search",
+            sx: { borderRadius: 4 },
+          }}
         />
-      </Search>
-    </>
+      )}
+    />
   );
 };
 
 export default SearchBar;
+
+const top100Films = [
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+  { title: "12 Angry Men", year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Pulp Fiction", year: 1994 },
+];
